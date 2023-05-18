1930e54f301c51931bdf20d08228d27143a552cc
hapizd <hapizdasari@gmail.com>
2023-05-18 15:28:12 +0700

    update to language react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,22 +8,22 @@ import TWITCH from "../image/icon_twitch.png";
 
 const Footer =()=>{
     return(
-        <div class="footer">
-        <div class="footer_utama">
-          <div class="footer_alamat">
+        <div className="footer">
+        <div className="footer_utama">
+          <div className="footer_alamat">
             <p>Jalan Suroyo No. 161 Mayangan Kota <br />Probolonggo 672000</p>
             <p>binarcarrental@gmail.com</p>
             <p>081-233-334-808</p>
           </div>
 
-          <div class="footer_link">
-            <a class="nav-link active" aria-current="page" href="#Our-Services-Section">Our Services</a>
-            <a class="nav-link" href="#Why-Us-Section">Why Us</a>
-            <a class="nav-link" href="#Testimonial-Section">Testimonial</a>
-            <a class="nav-link" href="#FAQ-Section">FAQ</a>
+          <div className="footer_link">
+            <a className="nav-link active" aria-current="page" href="#Our-Services-Section">Our Services</a>
+            <a className="nav-link" href="#Why-Us-Section">Why Us</a>
+            <a className="nav-link" href="#Testimonial-Section">Testimonial</a>
+            <a className="nav-link" href="#FAQ-Section">FAQ</a>
           </div>
 
-          <div class="footer_sosmed">
+          <div className="footer_sosmed">
             <p>Connect with us</p>
             <a href=""><img src={FB} alt="" /></a>
             <a href=""><img src={IG} alt="" /></a>
@@ -32,7 +32,7 @@ const Footer =()=>{
             <a href=""><img src={TWITCH} alt="" /></a>
           </div>
 
-          <div class="footer_copy">
+          <div className="footer_copy">
             <p>Copyright Binar 2022</p>
             <a href=""><img src="images/logo.png" alt="" /></a>
           </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,18 +11,18 @@ const Hero = () => {
   }, []);
   return (
     <>
-    <div class="desc_sewa" id="Hero-Section">
-        <div class="container-fluid desc_utama">
+    <div className="desc_sewa" id="Hero-Section">
+        <div className="container-fluid desc_utama">
   
-          <div class="desc_kiri">
+          <div className="desc_kiri">
             <h1>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
             <p>Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
-            <Link to="/cars"><button class="btn btn-success my-2 my-sm-0">Mulai Sewa Mobil</button></Link> 
+            <Link to="/cars"><button className="btn btn-success my-2 my-sm-0">Mulai Sewa Mobil</button></Link> 
           </div>
   
-          <div class="desc_kanan">
-            <img src={carImg} alt="" class="img-mobil" />
-            <div class="bg-mobil"></div>
+          <div className="desc_kanan">
+            <img src={carImg} alt="" className="img-mobil" />
+            <div className="bg-mobil"></div>
           </div>
   
         </div>
